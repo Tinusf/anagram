6d47b2c192018ea82d3648a02a63066749fe51f9
Tinus Flagstad <tinus@flagstad.as>
2019-09-27 11:27:05 +0200

    Add simple anagram algorithm, not super effective yet.

--- anagram.py
+++ anagram.py
@@ -17,12 +17,50 @@ def sort_words(word_list):
     return ["".join(sorted(word)) for word in word_list]
 
 
-def main():
-    # Read the eventyr.txt file.
-    word_list = read_file("eventyr.txt")
+def find_anagram(word_list):
+    """
+    :param word_list: A list containing many words.
+    :return: A list containing all the anagrams. Example: [["listen", "silent"]]
+    """
     # Sort the file.
     word_list_sorted = sort_words(word_list)
-    print(word_list_sorted)
+    # This is a list containing all the sorted words that are already used.
+    sorted_words_already_used = []
+    # A list containing all the anagrams.
+    anagrams = []
+    for i in range(len(word_list)):
+        current_sorted_word = word_list_sorted[i]
+        if current_sorted_word in sorted_words_already_used:
+            # If this word has already been checked then we can skip it.
+            continue
+        else:
+            sorted_words_already_used.append(current_sorted_word)
+
+        current_anagram = []
+        for j in range(len(word_list)):
+            if j == i:
+                # Skip the same word.
+                continue
+            # If the sorted words are equal then they are anagrams.
+            if current_sorted_word == word_list_sorted[j]:
+                # Append the non-sorted word.
+                current_anagram.append(word_list[j])
+        # If any anagrams was found.
+        if len(current_anagram) > 0:
+            current_anagram.insert(0, word_list[i])
+            anagrams.append(current_anagram)
+    return anagrams
+
+
+def main():
+    # TODO: after you have used one word you can safely remove it, it's impossible to be in
+    #  multiple anagrams at the same time.
+    # Read the eventyr.txt file.
+    word_list = read_file("eventyr.txt")
+
+    anagrams = find_anagram(word_list)
+    for anagram in anagrams:
+        print(", ".join(anagram))
 
 
 if __name__ == '__main__':
